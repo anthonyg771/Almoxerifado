22d6ca9e926180d500298279de070556042cf79d
Antony Vilanova <attony157@gmail.com>
2025-01-22 16:54:29 -0300

    Atualizar git iguinore

---
+++ BackSRC/AlmoxerifadoSLN/AlmoxerifadoAPI/Controllers/CategoriaController.cs
@@ -0,0 +1,39 @@
+﻿using AlmoxerifadoAPI.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace AlmoxerifadoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly List<Categoria> _categorias;
+        public CategoriaController() 
+        {
+
+        }
+        [HttpGet(Name = "lista")]
+        public ActionResult<Categoria> GetCategorias()
+        {
+            _categorias = new List<Categoria>()
+            new Categoria
+            {
+                Codigo = 1,
+                Descricao = "Alimentos"
+            },
+            new Categoria()
+            {
+                Codigo = 2,
+                Descricao = "Bebidas"
+            }
+        };
+        return Ok(_categorias);
+            
+        
+      
+
+           
+    }
+}
---
+++ BackSRC/AlmoxerifadoSLN/AlmoxerifadoAPI/Model/Categoria.cs
@@ -0,0 +1,8 @@
+﻿namespace AlmoxerifadoAPI.Model
+{
+    public class Categoria
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set;}
+    }
+}
